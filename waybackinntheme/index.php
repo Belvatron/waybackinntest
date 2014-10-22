e7784ad6a08dc564c4d6eaa64a7fe52f2cbcdbdd
Belvatron <r4gn4rkv3rn@gmail.com>
2014-10-21 20:17:37 -0700

    Functions and index changes

--- waybackinntheme/index.php
+++ waybackinntheme/index.php
@@ -1,6 +1,22 @@
 <?php
-echo "this is max";
-
-
-
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php bloginfo("name");?></title>
+	<link rel="stylesheet" href= "<?php bloginfo('stylesheet_url'); ?>">
+	
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<header id="main_header">
+		<hgroup>
+			<h1><a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo("name"); ?></a></h1>
+			<h2><?php bloginfo("description"); ?></h2>
+		</hgroup>	
+	</header>
+</body>
+</html>
 ?>
